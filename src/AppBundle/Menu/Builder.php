004aaa33e5cd0860989cc283ad1b02bded06676b
AlphaNetjerikhet <alpha.netjerikhet@gmail.com>
2017-01-18 13:50:58 +0100

    Fixing style for navigation Adding a class to the navbar in the Builder.php Making current menu item highlighted Modifying site main label (first item in the navbar)

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -7,6 +7,7 @@ use Knp\Menu\MenuFactory;
 class Builder {
 	public function mainMenu(MenuFactory $factory, array $options) {
 		$menu = $factory->createItem('root');
+		$menu->setChildrenAttribute('class', 'nav navbar-nav');
 		$menu->addChild('Home', ['route' => 'homepage']);
 		return $menu;
 	}
