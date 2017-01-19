c6ba39f33c885b5c2134f7cc9477c17a89ebf2c3
AlphaNetjerikhet <alpha.netjerikhet@gmail.com>
2017-01-19 15:42:12 +0100

    Generate CarBundle:Car entity CarRepository is generated Use respository to retreive data in the default controller Comment out the array of cars that was used earlier

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -10,11 +10,13 @@ class DefaultController extends Controller {
 	 * @Route("/our-cars", name="offer")
 	 */
 	public function indexAction() {
-		$cars = [
-			['Make' => 'BMW', 'Name' => 'X1'],
-			['Make' => 'Fiat', 'Name' => 'Croma'],
-			['Make' => 'Audi', 'Name' => 'Q7'],
-		];
+		// $cars = [
+		// 	['Make' => 'BMW', 'Name' => 'X1'],
+		// 	['Make' => 'Fiat', 'Name' => 'Croma'],
+		// 	['Make' => 'Audi', 'Name' => 'Q7'],
+		// ];
+		$carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
+		$cars = $carRepository->findAll();
 		return $this->render('CarBundle:Default:index.html.twig', ['cars' => $cars]);
 	}
 }
