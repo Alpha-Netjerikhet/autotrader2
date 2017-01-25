a159035a3750d1e8d3d9aad5f858f65d0eadc7d0
AlphaNetjerikhet <alpha.netjerikhet@gmail.com>
2017-01-25 16:40:00 +0100

    Add request object to the controller and handle it Add constraints to search field Display error message in view

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -5,12 +5,15 @@ namespace CarBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller {
 	/**
 	 * @Route("/our-cars", name="offer")
 	 */
-	public function indexAction() {
+	public function indexAction(Request $request) {
 		// $cars = [
 		// 	['Make' => 'BMW', 'Name' => 'X1'],
 		// 	['Make' => 'Fiat', 'Name' => 'Croma'],
@@ -20,8 +23,15 @@ class DefaultController extends Controller {
 		$cars = $carRepository->findCarsWithDetails(); // custom method instead of findAll()
 		$form = $this->createFormBuilder()
 			->setMethod('GET')
-			->add('search', TextType::class) // name and type of the field
+			->add('search', TextType::class, ['constraints' => [new NotBlank(), new Length(['min' => 2])]]) // name and type of the field
 			->getForm(); // to get the form from the builder
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			die('Form submitted');
+		}
+
 		return $this->render('CarBundle:Default:index.html.twig',
 			[
 				'cars' => $cars,
