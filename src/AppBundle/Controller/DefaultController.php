7b946549302bd044f50b4b6010cfa75fac61dc23
AlphaNetjerikhet <alpha.netjerikhet@gmail.com>
2017-01-26 13:45:51 +0100

    Add a search field to main page Generate a form for Make entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,16 +4,31 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller {
 	/**
 	 * @Route("/", name="homepage")
 	 */
 	public function indexAction(Request $request) {
-		// replace this example code with whatever you need
+		// create search field on main page
+		$form = $this->createFormBuilder()
+			->setMethod('GET')
+			->add('search', TextType::class, ['constraints' => [new NotBlank(), new Length(['min' => 2, 'max' => 16])]])
+			->getForm();
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			die('Form is valid and submitted!');
+		}
+
 		return $this->render('default/index.html.twig', [
 			'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+			'form' => $form->createView(),
 		]);
 	}
 }
