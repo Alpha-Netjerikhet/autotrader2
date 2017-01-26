8a7695d890dab8eb42f69029d2ce262ef9050b71
AlphaNetjerikhet <alpha.netjerikhet@gmail.com>
2017-01-26 17:00:17 +0100

    Generate form for entity Add --toString methods to Make & Model entities

--- src/CarBundle/Entity/Make.php
+++ src/CarBundle/Entity/Make.php
@@ -11,105 +11,100 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="make")
  * @ORM\Entity(repositoryClass="CarBundle\Repository\MakeRepository")
  */
-class Make
-{
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+class Make {
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255)
-     */
-    private $name;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="name", type="string", length=255)
+	 */
+	private $name;
 
-    /**
-     * @var ArrayCollection // doctrine uses ArrayCollection not array directly
-     *
-     * @ORM\OneToMany(targetEntity="CarBundle\Entity\Car", mappedBy="make")
-     */
-    private $cars;
+	/**
+	 * @var ArrayCollection // doctrine uses ArrayCollection not array directly
+	 *
+	 * @ORM\OneToMany(targetEntity="CarBundle\Entity\Car", mappedBy="make")
+	 */
+	private $cars;
 
+	/**
+	 * Get id
+	 *
+	 * @return int
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Set name
+	 *
+	 * @param string $name
+	 *
+	 * @return Make
+	 */
+	public function setName($name) {
+		$this->name = $name;
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return Make
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get name
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
 
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->cars = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->cars = new \Doctrine\Common\Collections\ArrayCollection();
+	}
 
-    /**
-     * Add car
-     *
-     * @param \CarBundle\Entity\Car $car
-     *
-     * @return Make
-     */
-    public function addCar(\CarBundle\Entity\Car $car)
-    {
-        $this->cars[] = $car;
+	/**
+	 * Add car
+	 *
+	 * @param \CarBundle\Entity\Car $car
+	 *
+	 * @return Make
+	 */
+	public function addCar(\CarBundle\Entity\Car $car) {
+		$this->cars[] = $car;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Remove car
-     *
-     * @param \CarBundle\Entity\Car $car
-     */
-    public function removeCar(\CarBundle\Entity\Car $car)
-    {
-        $this->cars->removeElement($car);
-    }
+	/**
+	 * Remove car
+	 *
+	 * @param \CarBundle\Entity\Car $car
+	 */
+	public function removeCar(\CarBundle\Entity\Car $car) {
+		$this->cars->removeElement($car);
+	}
 
-    /**
-     * Get cars
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getCars()
-    {
-        return $this->cars;
-    }
+	/**
+	 * Get cars
+	 *
+	 * @return \Doctrine\Common\Collections\Collection
+	 */
+	public function getCars() {
+		return $this->cars;
+	}
+
+	public function __toString() {
+		return $this->getName();
+	}
 }
